fbfdbf30ca55b4496d3b3f0a3b4b441ecc4be08e
Parkman97 <Parkmans@oregonstate.edu>
2024-01-03 17:31:37 -0500

    Updated Version

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Link } from 'react-scroll';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header className="App-header">
+      <h1>Hi, My name is Samuel Parkman</h1>
+    </header>
+    <img src={process.env.PUBLIC_URL + '/personal_photo.jpg'} alt='' id='personal-photo' />
+    <img src={process.env.PUBLIC_URL + '/mountains.jpg'} alt='' id='background-photo' />  
+
+
+
+    <main>
+      
+      <section className="App-article">
+      <Link to="App-header" smooth duration={500} offset={-100}>Top of Page</Link>
+        <Link to="about-me" smooth duration={500} offset={-100}>About Me</Link>
+        <Link to="projects" smooth duration={500} offset={-100}>Projects</Link>
+        <Link to="social-media" smooth duration={500} offset={-100}>Social Media</Link>
+        <Link to="contact-info" smooth duration={500} offset={-100}>Contact Info</Link>
+      </section>
+
+      <section className='about-me' id='about-me'>
+        <h2>About Me</h2>
+          <p>
+            I originally graduated with a B.S. in Mechanical Engineering from Washington State University but after working for 3 years
+            I found that I really enjoyed coding and decided to get another B.S but in Computer Science. Currently I am working to finish
+            up my degree which is expected to be in the spring of 2025. I am currently working as a Software Automation Engineer at General
+            Motors and honing my CS skills. 
+          </p>
+      </section>
+
+      <section className='contact-info' id='contact-info'>
+      <h2>Contact Info</h2>
+          <table>
+              <thead>
+              <tr>
+                  <th>Contact Method</th>
+                  <th>Info</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                  <td>Email</td>
+                  <td>swparkman02@gmail.com</td>
+              </tr>
+              <tr>
+                  <td>Phone</td>
+                  <td>(509) 713 - 4895</td>
+              </tr>
+              </tbody>
+          </table>
+      </section>
+
+      <section className='projects' id='projects'>
+        <h2>Projects</h2>
+      </section>
+      <section>
+        <img src='../public/Resume.jpg' alt='' id='logo' className='Resume'></img>
+      </section>
+
+      <section className='social-media' id='social-media'>
+        <h2>Social Media</h2>
+          <a href = 'https://www.linkedin.com/in/samuel-parkman/'> 
+          LinkedIn  
+          </a>
+      </section>
+
+    </main>
+  
+
+    <footer>
+      <p>&copy; 2023 Sam Parkman</p>
+    </footer>
+
+    </>
   );
 }
 
